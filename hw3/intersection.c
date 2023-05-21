d8128ede79063eb0cddcb9f1ada13001cbc90113
Anu <mindtheorigin@gmail.com>
2023-05-21 22:13:36 +0900

    First Complete hw3

--- hw3/intersection.c
+++ hw3/intersection.c
@@ -1,89 +1,166 @@
+/* May 21, 2023
+ * OS 401
+ * Homework 3
+ * Woohyuk An, Mathematics, 201902097
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-void* block(void* block);
+#define EAST 0
+#define WEST 1
+#define SOUTH 2
+#define NORTH 3
+
+void* thread_go_north(void* i);
+void* thread_go_south(void* i);
+void* thread_go_east(void* i);
+void* thread_go_west(void* i);
 
 sem_t sem[4];
+/*
+ 1 | 0
+ -----
+ 2 | 3
+ */
 
-int main() {
+int main(void) {
 
     for (int i=0; i<4; i++) {
         sem_init(&sem[i], 0, 1);
     }
 
-    pthread_t car[4][3];
-    // 0: go down, 1: go right, 2: go up, 3: go left
+	pthread_t car_thread[4][3];
+    /* FIRST INDEX: EAST 0, WEST 1, SOUTH 2, NORTH 3
+     * SECOND INDEX: 0, 1, 2 -th car from each direction
+     */
 
-    for (int i=0; i<4; i++) {
-        for (int j=0; j<3; j++) {
-
-            if (i==0) {
-                // block2 -> block3
-                pthread_create(&car[i][j], NULL, block, (void *) 2);
-                pthread_create(&car[i][j], NULL, block, (void *) 3);
-                
-            }
-            else if (i==1) {
-                // block2 -> block 3 -> block 4
-                pthread_create(&car[i][j], NULL, block, (void *) 2);
-                pthread_create(&car[i][j], NULL, block, (void *) 3);
-                pthread_create(&car[i][j], NULL, block, (void *) 4);
-                
-            }
-            else if (i==2) {
-                // block 4 -> block 1
-                pthread_create(&car[i][j], NULL, block, (void *) 4);
-                pthread_create(&car[i][j], NULL, block, (void *) 1);
-                pthread_join(car[i][j], NULL);
-                pthread_join(car[i][j], NULL);
-            }
-            else if (i==3) {
-                // block 4 -> block 1 -> block 2
-                pthread_create(&car[i][j], NULL, block, (void *) 4);
-                pthread_create(&car[i][j], NULL, block, (void *) 1);
-                pthread_create(&car[i][j], NULL, block, (void *) 2);
-                pthread_join(car[i][j], NULL);
-                pthread_join(car[i][j], NULL);
-                pthread_join(car[i][j], NULL);
-            }
-        }
+    for (int i=0; i<3; i++) {
+        int train_num = i+1;
+        int* intPtr = (int*)malloc(sizeof(int));
+        *intPtr = i+1;
+        pthread_create(&car_thread[0][i], NULL, thread_go_east, (void*)intPtr);
+        pthread_create(&car_thread[1][i], NULL, thread_go_west, (void*)intPtr);
+        pthread_create(&car_thread[2][i], NULL, thread_go_south, (void*)intPtr);
+        pthread_create(&car_thread[3][i], NULL, thread_go_north, (void*)intPtr);
     }
 
     for (int i=0; i<4; i++) {
-        for (int j=0; j<3; j++) {
-            pthread_join(car[i][j], NULL);
-        }
-    }
-    
-    for (int i=0; i<4; i++) {
-        sem_destroy(&sem[i]);
+        pthread_join(car_thread[0][i], NULL);
+        pthread_join(car_thread[1][i], NULL);
+        pthread_join(car_thread[2][i], NULL);
+        pthread_join(car_thread[3][i], NULL);
     }
 
+	sem_destroy(&sem[0]);
+	sem_destroy(&sem[1]);
+	sem_destroy(&sem[2]);
+	sem_destroy(&sem[3]);
+
+	return 0;
 }
 
-/* block
- * 2 | 1
- * -----
- * 3 | 4
- */
 
-void* block(void* block) {
+void* thread_go_north(void* i) {
+	int train_num = *((int*)i);
 
-    int block_num = *((int *)block);
+    // Block: 3 -> 0
+    sem_wait(&sem[3]);
+    sem_wait(&sem[0]);
 
-    // wait P
-    sem_wait(&sem[block_num]);
-    printf("BLOCK %d has been locked\n", block_num);
+    printf("A car from south no.%d has been to pass through the intersection\n", train_num);
 
-    // critical section
+    /* Critical Section */
     sleep(1);
-    printf("BLOCK %d has been released\n", block_num);
+    printf("A car from south no.%d has been passed through the intersection\n", train_num);
 
-    // V
-    sem_post(&sem[block_num]);
-    free(block);
+    sem_post(&sem[0]);
+    sem_post(&sem[3]);
+
+    free(i);
 }
 
+void* thread_go_south(void* i) {
+	int train_num = *((int*)i);
+
+    // Block 1 -> 2
+    sem_wait(&sem[1]);
+    sem_wait(&sem[2]);
+
+    printf("A car from north no.%d has been to pass through the intersection\n", train_num);
+
+    /* Critical Section */
+    sleep(1);
+    printf("A car from north no.%d has been passed through the intersection\n", train_num);
+
+    sem_post(&sem[1]);
+    sem_post(&sem[2]);
+
+    free(i);
+}
+
+void* thread_go_east(void* i) {
+    int train_num = *((int*)i);
+
+    // Block 1 -> 2 -> 3
+    sem_wait(&sem[1]);
+    sem_wait(&sem[2]);
+    sem_wait(&sem[3]);
+
+    printf("A car from North no.%d has turned left passing through the intersection\n", train_num);
+
+    /* Critical Section */
+    sleep(1);
+    printf("A car from North no.%d has been turned left passing through the intersection\n", train_num);
+
+    sem_post(&sem[1]);
+    sem_post(&sem[2]);
+    sem_post(&sem[3]);
+
+    free(i);
+}
+
+void* thread_go_west(void* i) {
+    int train_num = *((int*)i);
+
+    // Block 3 -> 0 -> 1
+    sem_wait(&sem[3]);
+    sem_wait(&sem[0]);
+    sem_wait(&sem[1]);
+
+    printf("A car from South no.%d has turned left passing through the intersection\n", train_num);
+
+    /* Critical Section */
+    sleep(1);
+    printf("A car from South no.%d has been turned left passing through the intersection\n", train_num);
+
+    sem_post(&sem[3]);
+    sem_post(&sem[0]);
+    sem_post(&sem[1]);
+
+    free(i);
+}
+
+void* thread_go_east(void* i) {
+    int train_num = *((int*)i);
+
+    // Block 1 -> 2 -> 3
+    sem_wait(&sem[1]);
+    sem_wait(&sem[2]);
+    sem_wait(&sem[3]);
+
+    printf("A car from North no.%d has turned left passing through the intersection\n", train_num);
+
+    /* Critical Section */
+    sleep(1);
+    printf("A car from North no.%d has been turned left passing through the intersection\n", train_num);
+
+    sem_post(&sem[1]);
+    sem_post(&sem[2]);
+    sem_post(&sem[3]);
+
+    free(i);
+}
